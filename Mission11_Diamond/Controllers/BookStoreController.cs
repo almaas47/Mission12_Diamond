eb67480a7338d555108ca4b3e68fa5ecc121be53
almaas47 <matt47@byu.edu>
2025-03-25 16:16:57 -0600

    Phase4 commit

--- Mission11_Diamond/Controllers/BookStoreController.cs
+++ Mission11_Diamond/Controllers/BookStoreController.cs
@@ -13,14 +13,31 @@ namespace Mission11_Diamond.Controllers
         public BookStoreController(BookstoreDbContext temp) => _bookContext = temp;
 
         [HttpGet]
-        public IActionResult GetBooks(int pageHowMany = 5, int pageNumber = 0)
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNumber = 0, [FromQuery] List<string>? projectTypes = null)
         {
-            var something = _bookContext.Books
+            var query = _bookContext.Books.AsQueryable();
+            if (projectTypes != null && projectTypes.Any())
+            {
+                query = query.Where(p => projectTypes.Contains(p.Category));
+            }
+            
+            string? favoriteBook = Request.Cookies["FavoriteBook"];
+            Console.WriteLine("==========COOKIE==========");
+            
+            HttpContext.Response.Cookies.Append("FavoriteBook", "Les Miserables", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.Now.AddMinutes(1)
+            });
+            
+            var totalNumBooks = query.Count();
+            
+            var something = query
                 .Skip((pageNumber - 1) * pageHowMany)
                 .Take(pageHowMany)
-                .ToList();   
-            
-            var totalNumBooks = _bookContext.Books.Count();
+                .ToList();
 
             var theObject = new
             {
@@ -30,8 +47,16 @@ namespace Mission11_Diamond.Controllers
             
             return Ok(theObject);
         }
+
+        [HttpGet("BookTypes")]
+        public IActionResult GetBookTypes()
+        {
+            var bookTypes = _bookContext.Books
+                .Select(p => p.Category)
+                .Distinct()
+                .ToList();
+            return Ok(bookTypes);
+        }
         
-        // [HttpGet]
-        // public IEnumerable<Book> GetFunctionalBooks() => _bookContext.Book.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
     }
 }
